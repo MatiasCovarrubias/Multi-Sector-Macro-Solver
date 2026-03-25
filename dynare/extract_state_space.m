be385e34dc0cd09028eb8a00debdd97d2d735f1c
MatiasCovarrubias <matias.covarrubias@nyu.edu>
2026-03-25 14:08:27 +0100

    Fix state space extraction (matrix C)

--- dynare/extract_state_space.m
+++ dynare/extract_state_space.m
@@ -1,26 +1,30 @@
 function SolData = extract_state_space(oo_, M_, n_sectors, policies_ss)
     dim = n_sectors;
     inv = oo_.dr.inv_order_var;
+    idx = get_variable_indices(n_sectors);
 
     %% Variable index ranges in Dynare ordering
-    k_ind      = [1,        dim];
-    a_ind      = [dim+1,    2*dim];
-    c_ind      = [2*dim+1,  3*dim];
-    l_ind      = [3*dim+1,  4*dim];
-    pk_ind     = [4*dim+1,  5*dim];
-    pm_ind     = [5*dim+1,  6*dim];
-    m_ind      = [6*dim+1,  7*dim];
-    mout_ind   = [7*dim+1,  8*dim];
-    i_ind      = [8*dim+1,  9*dim];
-    iout_ind   = [9*dim+1,  10*dim];
-    p_ind      = [10*dim+1, 11*dim];
-    q_ind      = [11*dim+1, 12*dim];
-    y_ind      = [12*dim+1, 13*dim];
-    cagg_ind   = 13*dim+1;
-    lagg_ind   = 13*dim+2;
-    yagg_ind   = 13*dim+3;
-    iagg_ind   = 13*dim+4;
-    magg_ind   = 13*dim+5;
+    k_ind      = idx.k;
+    a_ind      = idx.a;
+    c_ind      = idx.c;
+    l_ind      = idx.l;
+    pk_ind     = idx.pk;
+    pm_ind     = idx.pm;
+    m_ind      = idx.m;
+    mout_ind   = idx.mout;
+    i_ind      = idx.i;
+    iout_ind   = idx.iout;
+    p_ind      = idx.p;
+    q_ind      = idx.q;
+    y_ind      = idx.y;
+    c_util_ind = idx.c_util;
+    l_util_ind = idx.l_util;
+    cagg_ind   = idx.c_agg;
+    lagg_ind   = idx.l_agg;
+    gdpagg_ind = idx.gdp_agg;
+    iagg_ind   = idx.i_agg;
+    kagg_ind   = idx.k_agg;
+    utility_intratemp_ind = idx.utility_intratemp;
 
     %% Map to Dynare internal ordering
     k_inv      = [inv(k_ind(1)),    inv(k_ind(2))];
@@ -36,11 +40,14 @@ function SolData = extract_state_space(oo_, M_, n_sectors, policies_ss)
     p_inv      = [inv(p_ind(1)),    inv(p_ind(2))];
     q_inv      = [inv(q_ind(1)),    inv(q_ind(2))];
     y_inv      = [inv(y_ind(1)),    inv(y_ind(2))];
+    c_util_inv = inv(c_util_ind);
+    l_util_inv = inv(l_util_ind);
     cagg_inv   = inv(cagg_ind);
     lagg_inv   = inv(lagg_ind);
-    yagg_inv   = inv(yagg_ind);
+    gdpagg_inv = inv(gdpagg_ind);
     iagg_inv   = inv(iagg_ind);
-    magg_inv   = inv(magg_ind);
+    kagg_inv   = inv(kagg_ind);
+    utility_intratemp_inv = inv(utility_intratemp_ind);
 
     %% State transition: S(t) = A*S(t-1) + B*e(t)
     ghx = oo_.dr.ghx;
@@ -52,7 +59,16 @@ function SolData = extract_state_space(oo_, M_, n_sectors, policies_ss)
     %% Policy functions: X(t) = C*S(t-1) + D*e(t)
     pol_rows_x = [c_inv; l_inv; pk_inv; pm_inv; m_inv; mout_inv; ...
                   i_inv; iout_inv; p_inv; q_inv; y_inv];
-    agg_rows = [cagg_inv; lagg_inv; yagg_inv; iagg_inv; magg_inv];
+    agg_rows = [
+        c_util_inv;
+        l_util_inv;
+        cagg_inv;
+        lagg_inv;
+        gdpagg_inv;
+        iagg_inv;
+        kagg_inv;
+        utility_intratemp_inv
+    ];
 
     C_blocks = cell(size(pol_rows_x, 1) + numel(agg_rows), 1);
     D_blocks = cell(size(pol_rows_x, 1) + numel(agg_rows), 1);
@@ -71,6 +87,11 @@ function SolData = extract_state_space(oo_, M_, n_sectors, policies_ss)
     C = vertcat(C_blocks{:});
     D = vertcat(D_blocks{:});
 
+    assert(size(C, 1) == numel(policies_ss), ...
+        'extract_state_space:PolicyDimensionMismatch', ...
+        'StateSpace.C has %d rows but policies_ss has %d elements.', ...
+        size(C, 1), numel(policies_ss));
+
     %% Pack output
     SolData = struct();
     SolData.A = A;
@@ -82,6 +103,7 @@ function SolData = extract_state_space(oo_, M_, n_sectors, policies_ss)
     SolData.indices = struct('k', k_ind, 'a', a_ind, 'c', c_ind, 'l', l_ind, ...
         'pk', pk_ind, 'pm', pm_ind, 'm', m_ind, 'mout', mout_ind, ...
         'i', i_ind, 'iout', iout_ind, 'p', p_ind, 'q', q_ind, 'y', y_ind, ...
-        'cagg', cagg_ind, 'lagg', lagg_ind, 'yagg', yagg_ind, ...
-        'iagg', iagg_ind, 'magg', magg_ind);
+        'c_util', c_util_ind, 'l_util', l_util_ind, 'c_agg', cagg_ind, ...
+        'l_agg', lagg_ind, 'gdp_agg', gdpagg_ind, 'i_agg', iagg_ind, ...
+        'k_agg', kagg_ind, 'utility_intratemp', utility_intratemp_ind);
 end
